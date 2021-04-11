3fd0cb001519e6463108c968bbdc491ae712e936
Victor Navarro <victor@vimtor.io>
2021-04-11 21:34:34 +0200

    chore(browser-extension): run prettier

--- packages/filant-browser-extension/background.js
+++ packages/filant-browser-extension/background.js
@@ -1,11 +1,11 @@
 chrome.contextMenus.create({
-    id: "1",
-    title: "Open in editor",
-    contexts:["all"]
-});
+  id: '1',
+  title: 'Open in editor',
+  contexts: ['all'],
+})
 
-chrome.contextMenus.onClicked.addListener((data, tab)=>{
-    chrome.tabs.sendMessage(tab.id,"getElement", (response =>{
-        console.log(response);
-    } ))
+chrome.contextMenus.onClicked.addListener((data, tab) => {
+  chrome.tabs.sendMessage(tab.id, 'getElement', response => {
+    console.log(response)
+  })
 })
--- packages/filant-browser-extension/content.js
+++ packages/filant-browser-extension/content.js
@@ -1,14 +1,14 @@
-let element = null;
+let element = null
 
-document.addEventListener("contextmenu", (event)=>{
-    element = event.target
-    console.log(element)
-});
+document.addEventListener('contextmenu', event => {
+  element = event.target
+  console.log(element)
+})
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse)=>{
-    console.log(element);
-    if(message == "getElement"){
-        sendResponse({data: element.toString()})
-    }
-    return true
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log(element)
+  if (message == 'getElement') {
+    sendResponse({ data: element.toString() })
+  }
+  return true
 })
