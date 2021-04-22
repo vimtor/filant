aa27a8749d7ad6711e47375d148908868fd7ac02
ToniRealp <tonirealp@gmail.com>
2021-04-22 11:55:31 +0200

    feat(browser-extension): ide explanation logic

--- packages/filant-browser-extension/popup/index.js
+++ packages/filant-browser-extension/popup/index.js
@@ -2,6 +2,7 @@ const ideButtons = document.querySelectorAll('.ide__item')
 const instructionForwardButton = document.querySelector('[aria-label="see instructions"]')
 const instructionsBackButton = document.querySelector('.instructions__back')
 const instructionsSection = document.querySelector('.instructions')
+const ideExplanations = document.querySelectorAll('.explanation__item--hidden')
 
 chrome.storage.sync.get('ide', ({ ide: selectedIde }) => {
   ideButtons.forEach(ideButton => {
@@ -9,13 +10,28 @@ chrome.storage.sync.get('ide', ({ ide: selectedIde }) => {
       ideButton.classList.add('selected')
     }
   })
+  ideExplanations.forEach(ideExplanation =>{
+    if (ideExplanation.getAttribute('data-ide') === selectedIde) {
+      ideExplanation.classList.remove('explanation__item--hidden')
+    }
+  })
 })
 
 ideButtons.forEach(ideButton => {
   ideButton.addEventListener('click', () => {
     ideButtons.forEach(ideButton => ideButton.classList.remove('selected'))
+    ideExplanations.forEach(ideExplanation => ideExplanation.classList.add('explanation__item--hidden'))
     ideButton.classList.add('selected')
     chrome.storage.sync.set({ ide: ideButton.getAttribute('data-ide') })
+  })
+})
+
+chrome.storage.onChanged.addListener((changes,_)=>{
+  const selectedIde = changes.ide.newValue
+  ideExplanations.forEach(ideExplanation =>{
+    if (ideExplanation.getAttribute('data-ide') === selectedIde) {
+      ideExplanation.classList.remove('explanation__item--hidden')
+    }
   })
 })
 
