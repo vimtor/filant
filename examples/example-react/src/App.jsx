2357a8d5ba6ec65317869a2a4fa3dd1294276e9b
Victor Navarro <victor@vimtor.io>
2021-04-23 21:01:19 +0200

    chore: only keep working examples

--- examples/example-react/src/App.jsx
+++ examples/example-react/src/App.jsx
@@ -1,26 +1,16 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function Button() {
-  return <button data-filant={undefined}>Without data attribute</button>
+  return <button data-filant={undefined}>Without Attribute</button>
 }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <Button />
+      <p>
+        A paragraph with <code>nested code</code>
+      </p>
     </div>
   )
 }
